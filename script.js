6b0cdb3bc4115d98a803d32b339900c51e3bc1fb
ZKibanov <68987481+ZKibanov@users.noreply.github.com>
2020-12-21 17:24:05 +0300

    ver 1.0 with bugs

--- script.js
+++ script.js
@@ -7,77 +7,58 @@ function getPost(name) {
 		})
     }
 
-    const debounce = (fn, debounceTime) => {
-            let timer;
-        function callback (){
-            let func = () => {fn.apply(this,arguments)};
-            clearTimeout(timer);
-            timer = setTimeout(func, debounceTime);
-            };
-        return callback
+const debounce = (fn, debounceTime) => {
+        let timer;
+    function callback (){
+        let func = () => {fn.apply(this,arguments)};
+        clearTimeout(timer);
+        timer = setTimeout(func, debounceTime);
         };
+        return callback
+    };
         
-    
+    function elementCreator(tag, cl, textContent=null) {
+        const elem = document.createElement(tag);
+        if (cl) elem.classList.add(cl);
+        elem.textContent = textContent;
+        return elem
+    };    
 
 
 const container = document.querySelector('.container')
 
 function cardTemplate (el) {
-    const card = document.createElement('div')
-    card.classList.add('card');
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
-    const article = document.createElement('p');
-    article.classList.add('card-text');
-    article.textContent = `Name:${el.name}`;
+    const card = elementCreator('div','card');
+
+    const cardBody = elementCreator('div','card-body');
+
+    const article = elementCreator('p','card-text', `Name:${el.name}`);
     cardBody.appendChild(article);
-    const article2 = document.createElement('p');
-    article2.textContent = `Owner:${el.owner.login}`;
+
+    const article2 = elementCreator('p','card-text', `Owner:${el.owner.login}`);
     cardBody.appendChild(article2);
-    const article3 = document.createElement('p');
-    article3.textContent = `Stars:${el.stargazers_count}`;
+
+    const article3 = elementCreator('p','card-text', `Stars:${el.stargazers_count}`);
     cardBody.appendChild(article3);
     card.appendChild(cardBody);
-    const btnDelete = document.createElement('button')
-    btnDelete.classList.add('button-delete')
-    btnDelete.textContent = 'X';
+
+    const btnDelete = elementCreator('button','button-delete', 'x');
     btnDelete.onclick = function(){
         btnDelete.parentElement.remove();        
     }
+
     card.appendChild(btnDelete)
     return card
     }
 
-
-
 function showSearchResults (searchResults) {
-    const resultsList = document.createElement('div')
-    resultsList.classList.add('resultsList');
+    const resultsList = elementCreator('div','resultsList')
     let resultsArray = searchResults.items;
-    const art = document.createElement('p');
-    art.number = 0;
-    art.textContent = resultsArray[0].name;
-    resultsList.appendChild(art);
- 
-    const art1 = document.createElement('p');
-    art1.number = 1;
-    art1.textContent = resultsArray[1].name;
-    resultsList.appendChild(art1);
-   
-    const art2 = document.createElement('p');
-    art2.number = 2;
-    art2.textContent = resultsArray[2].name;
-    resultsList.appendChild(art2);
-    
-    const art3 = document.createElement('p');
-    art3.number = 3;
-    art3.textContent = resultsArray[3].name;
-    resultsList.appendChild(art3);
-    
-    const art4 = document.createElement('p');
-    art4.number = 4;
-    art4.textContent = resultsArray[4].name;
-    resultsList.appendChild(art4);
+    for (let i = 0; i < 5;i += 1){
+        let art = elementCreator('p',null, resultsArray[i].name);
+        art.number = i;
+        resultsList.appendChild(art);
+    }
 
     resultsList.onclick = function(event){
         let target = event.target;
@@ -87,15 +68,13 @@ function showSearchResults (searchResults) {
 }
 
 function createPost(response){
+        const searchResults = document.querySelector('.resultsList')
+        searchResults.remove()
         let card = cardTemplate(response);
         input.value = '';
         container.insertAdjacentElement('beforeend',card);
-        const searchResults = document.querySelector('.resultsList')
-        searchResults.remove()
+
     };
-
-  
-
 
 async function searchRepos (searchReqest){ 
   let res = await getPost(searchReqest);
@@ -103,13 +82,12 @@ async function searchRepos (searchReqest){
 }
 
 function getSearchRequest() {
-    var searchRequest = input.value;
+    const searchRequest = input.value;
     const searchResults = document.querySelector('.resultsList')
     if (searchResults) searchResults.remove()
     searchRepos(searchRequest)
   }
 
-const input = document.querySelector('.input');
-const debouncedFn = debounce(getSearchRequest,1000)
-input.addEventListener('keyup', debouncedFn);
-
+const input = document.querySelector('.input')
+const debouncedFn = debounce(getSearchRequest,300)
+input.addEventListener('input', debouncedFn);
